2ff6cd88b94576586a751fc84c2bc193054e2df5
Guilherme Cesconeto <cesconeto@gmail.com>
2022-09-17 18:06:55 -0300

    feat: added update logic

--- src/models/universities/updateByID.js
+++ src/models/universities/updateByID.js
@@ -1,12 +1,13 @@
 const { ObjectId } = require('mongodb');
 const connection = require('../DBConnection');
+const { unprocessableEntity } = require('../../errors/errors');
 
-module.exports = async (id, updatedItem) => {
-  if (!ObjectId.isValid(id)) throw new Error('Invalid mongoDB Id');
+module.exports = async (id, fields) => {
+  if (!ObjectId.isValid(id)) throw unprocessableEntity;
   const db = await connection();
   const result = await db.collection('Universities').updateOne(
     { _id: ObjectId(id) },
-    { $set: updatedItem },
+    fields,
   );
   return result;
 };
--- src/services/universities/updateByID.js
+++ src/services/universities/updateByID.js
@@ -1,8 +1,17 @@
 const { updateById } = require('../../models/universities');
-const { dbError } = require('../../errors/errors');
+const { dbError, notFound } = require('../../errors/errors');
 
-module.exports = async (idToUpdate, updatedUniversity) => {
-  const result = await updateById(idToUpdate, updatedUniversity);
+module.exports = async (idToUpdate, updatedFields) => {
+  const fields = { $push: {}, $set: {} };
+
+  const { web_pages: pages, domains, name } = updatedFields;
+
+  if (pages) fields.$push.web_pages = { $each: pages };
+  if (domains) fields.$push.domains = { $each: domains };
+  if (name) fields.$set.name = name;
+
+  const result = await updateById(idToUpdate, fields);
   if (!result.acknowledged) throw dbError;
+  if (result.matchedCount === 0) throw notFound;
   return result;
 };
